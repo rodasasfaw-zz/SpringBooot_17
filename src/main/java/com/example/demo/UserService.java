305316651bdd8d1b8faa505cf28942112c9b15c5
rodasasfaw <rodasasfaw4@gmail.com>
2018-02-12 23:15:57 -0500

    Initial commit

---
+++ src/main/java/com/example/demo/UserService.java
@@ -0,0 +1,34 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+
+@Service
+public class UserService {
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    RoleRepository roleRepository;
+    @Autowired
+    public UserService(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+    public User findByEmail(String email){
+        return userRepository.findByEmail(email);
+    }
+    public Long countByEmail(String email){
+        return userRepository.countByEmail(email);
+    }
+    public User findByUserName(String username){
+        return userRepository.findByUserName(username);
+    }
+
+    public void saveUser(User user){
+        user.setRoles(Arrays.asList(roleRepository.findByRole("USER")));
+    }
+    public void saveAdmin(User user){
+        user.setRoles(Arrays.asList(roleRepository.findByRole("ADMIN")));
+    }
+}
